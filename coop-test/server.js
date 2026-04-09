c13001cb04daa3a0543d053e9d5966621b4a3855
Vinicius Antoniassi Salgado <vinicius.antoniassi27@gmail.com>
2026-04-09 15:57:42 -0300

    INITIAL

---
+++ coop-test/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to set the COOP header on all responses
+app.use((req, res, next) => {
+    // Change 'same-origin' to 'unsafe-none' later to see the difference
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    next();
+});
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`COOP Test Server running at http://localhost:${port}`);
+});
