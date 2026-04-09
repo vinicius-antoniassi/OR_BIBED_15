4e01091ddea0138b95fb000d2d853da6b4d16aa5
Vinicius Antoniassi Salgado <vinicius.antoniassi27@gmail.com>
2026-04-09 20:25:08 -0300

    Fix (we hope)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3000;
 // Middleware to set the COOP header on all responses
 app.use((req, res, next) => {
     // Change 'same-origin' to 'unsafe-none' later to see the difference
-    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
     next();
 });
 
